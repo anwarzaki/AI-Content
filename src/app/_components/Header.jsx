6fb5cce16efd42d5aed6c08e28f3419733519549
Zaki Anwar <157703298+anwarzaki@users.noreply.github.com>
2025-05-25 23:11:42 +0530

    Update Header.jsx

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -1,10 +1,17 @@
+"use client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { UserButton} from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 import React from "react";
 import "./Header.css";
 
 const Header = () => {
+   const router = useRouter();
+
+    const handleClick = () => {
+    router.push("/create-course");
+  };
   return (
     <div className=" nav flex bg-white justify-between items-center p-5 h-[10rem] shadow-md">
       <Image
@@ -16,7 +23,7 @@ const Header = () => {
         priority
       />
       <div className="flex items-center gap-5">
-      <Button>Get Started</Button>
+      <Button onClick={handleClick}>Get Started</Button>
       <UserButton />
       </div>
     </div>
